3bbd83a28493d6be408f6abdc099c43794bc5ed0
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-21 15:36:55 -0700

    Add header file

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int (*get_specifier(const char *))(va_list);
+int print_character(va_list);
+int print_string(va_list);
+int print_percentage(va_list);
+int print_decimal(va_list);
+int print_integer(va_list);
+
+
+/**
+ * struct func - struct for specifier to printer
+ * @t: character to compare
+ * @f: printer function
+ *
+*/
+
+typedef struct func
+{
+	char *t;
+	int (*f)(va_list);
+
+} func_t;
