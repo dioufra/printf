cfa7a6968a0546ccba47c6a866928b3705717f0d
Amynaby21 <105367983+Amynaby21@users.noreply.github.com>
2022-10-22 01:01:18 +0100

    Prints character

---
+++ print_character.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chr - print characters to stdout
+ * 
+ * @args: variadic parameters
+ * 
+ * Return: Number of character printed
+*/
+
+int print_chr(va_list args)
+{
+    /* iterate though arguments */
+    char c = (char)va_arg(args, int);
+
+    int count = 0;
+
+    if (c)
+    {
+        count = write(1, &c, 1);
+        return (count);
+
+    }
+    return (0);
+}
