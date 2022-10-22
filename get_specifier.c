a73fa9e32c45b88e4724f2d170f07e04afe3ec24
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-21 17:34:51 -0700

    first commit

--- get_specifier.c
+++ get_specifier.c
@@ -1,37 +1,33 @@
 #include "main.h"
 
 /**
- * get_specifier - check for valid specifier and 
- * call the appropriate function for printing
- * 
- * @format: format (char*)
- * 
- * Return: If success return a poiinter to a function
- *         Else, return NULL
-*/
+ * get_specifier - chek for valid specifier and
+ * call th appropriate function for printing
+ *
+ * @format: format
+ *
+ * Return: If success return a pointer to a function
+ * Else, return NULL
+ */
 
 int (*get_specifier(const char *format))(va_list)
 {
-    int i;
- 
-    func_t array[] = {
-        {"c", print_character},
-        {"s", print_string},
-        {"%", print_percentage},
-        {"i", print_integer},
-        {"d", print_decimal},
-        {NULL, NULL}
-    };
+	int i;
 
-    for (i = 0; array[i].t != NULL; i++) // t is c, s, % or NULL --- declared in main
-    {
-        if (*(array[i].t) == *format)
-        {
-            /* Return the corresponding function */
-            return (array[i].f);
-        }
-    }
+	func_t array[] = {
+		{"c", print_charcter},
+		{"s", print_string},
+		{"%", print_percentage},
+		{"i", print_integer},
+		{"d", print_decimal},
+		{NULL, NULL}
+	};
 
-    /* no match if found */
-    return (NULL);
+	for (i = 0; array[i].t != NULL; i++)
+	{
+		if (*(array[i].t) == *format)
+			return (array[i].f);
+	}
+
+	return (NULL);
 }
